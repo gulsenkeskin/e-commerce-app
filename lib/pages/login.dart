55ce2daf1c0aa0acf16f167fcabfd0d4c40b9e20
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-01 00:25:45 +0300

    password ve email text inputları eklendi
    :octocat:

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -17,19 +16,41 @@ class _LoginPageState extends State<LoginPage> {
         child: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 24.0),
           children: [
-            const SizedBox(height: 80.0,),
+            const SizedBox(
+              height: 80.0,
+            ),
             Column(
               children: [
                 Image.asset("assets/diamond.png"),
-                const SizedBox(height: 16.0,),
+                const SizedBox(
+                  height: 16.0,
+                ),
                 const Text("SHRINE"),
               ],
             ),
-            const SizedBox(height: 120.0,)
+            const SizedBox(
+              height: 120.0,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                //filled:Metin alanının arka planını insanlara yardım için doldurur
+                filled: true, labelText: 'Password',
+              ),
+              //obscureText: truedeğeri, kullanıcının yazdığı girişi otomatik olarak parolalara uygun madde işaretleri ile değiştirir.
+              obscureText: true,
+            )
           ],
         ),
       ),
     );
   }
 }
-

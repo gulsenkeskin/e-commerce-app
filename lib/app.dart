617ed4a4c8f27ca3274e3c19162d8f5272179d10
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-01 16:43:00 +0300

    added theme :octocat:

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,23 @@
+import 'package:e_commerce_app/colors.dart';
 import 'package:e_commerce_app/pages/home.dart';
 import 'package:e_commerce_app/pages/login.dart';
 import 'package:flutter/material.dart';
+
+final ThemeData _kShrineTheme=_buildShrineTheme();
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base= ThemeData.light();
+  return base.copyWith(
+    colorScheme: base.colorScheme.copyWith(
+      primary: kShrinePink100,
+      onPrimary: kShrineBrown900,
+      secondary: kShrineBrown900,
+      error: kShrineErrorRed,
+    )
+  );
+}
+
+
 
 class ShrineApp extends StatelessWidget {
   const ShrineApp({Key? key}) : super(key: key);
@@ -12,6 +29,7 @@ class ShrineApp extends StatelessWidget {
       home: const HomePage(),
       initialRoute: '/login',
       onGenerateRoute: _getRoute,
+      theme: _kShrineTheme,
     );
   }
 
